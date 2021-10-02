d9fcd5a6fcb5f5771e4a994038c24feb88f421a6
NatalyCP <natalycarvajalp@outlook.com>
2021-10-02 10:19:41 -0500

    Conexión a base de datos
    Creación de app de autenticación de usuarios y creación de primer modelo.

--- Vuelafacil/settings.py
+++ Vuelafacil/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "autenticacionUsuarios",
 ]
 
 MIDDLEWARE = [
@@ -75,9 +76,14 @@ WSGI_APPLICATION = 'Vuelafacil.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vuela-facil',
+        'USER': 'root',
+        'PASSWORD': 'recordarparaSQL',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
+
 }
 
 
