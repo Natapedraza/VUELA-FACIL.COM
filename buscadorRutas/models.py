0be19b8591200253483964542bfbd37796e6feea
NatalyCP <natalycarvajalp@outlook.com>
2021-10-07 16:06:33 -0500

    Modelo rutas
    Modelo base de datos.

--- buscadorRutas/models.py
+++ buscadorRutas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Ruta(models.Model):
+    codigoRuta = models.BigAutoField(primary_key=True)
+    ciudadOrigen = models.CharField(max_length=40)
+    ciudadDestino= models.CharField(max_length=40)
+    precio = models.FloatField()
+    fechaIda = models.DateField()
+    fechaVuelta = models.DateField()
+    descuento = models.FloatField()
