72a721308429e62236d46b620b541f52b607e665
andresalayon02 <90225771+andresalayon02@users.noreply.github.com>
2021-10-08 21:23:39 -0500

    Buscador rutas
    primer adelanto buscador rutas

--- buscadorRutas/urls.py
+++ buscadorRutas/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import RutasEncontradas
+from .views import RutasEncontradas, buscarRutas
 
 urlpatterns = [
     path("Encontradas/",RutasEncontradas.as_view()),
-    
+    path('Buscador/',buscarRutas),
 ]
--- buscadorRutas/views.py
+++ buscadorRutas/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, redirect, render
 from django.views.generic.list import ListView
-import os
+from .forms import Buscador
 from buscadorRutas.models import Ruta
 
 
 class RutasEncontradas(ListView):
    model = Ruta
    template_name = "home_login_rutas.html"
+
+def buscarRutas(request): 
+
+    if request.method == 'POST': 
+        buscador = Buscador(request.POST)
+        if buscador.is_valid:
+            datosBuscador = buscador.cl 
+            ciudad_origen = datosBuscador['ciudadOrigen']
+            ruta = get_list_or_404(Ruta,ciudadOrigen=ciudad_origen)
+            print(ruta)
+            request.session['ruta'] = {'ruta': ruta} 
+            return redirect('buscadorRuta/Encontradas/')
+    else :
+        buscador = Buscador()
+
+
+    return render(request, 'buscadorRutas.html', {'buscador':Buscador})
